8032cd6225cf42e2aee7acbbd8abd4125f26bff8
o.didenko <o.didenko@gaijin.ru>
2019-02-27 16:01:23 +0300

    refactored bottom_bar, added orientation blocking'

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:eyehelper/src/theme.dart';
 import 'package:eyehelper/src/routes.dart';
+import 'package:flutter/services.dart';
 
 
 class EyeHelperApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown
+    ]);
+
     return MaterialApp(
       title: 'Eyehelper',
       debugShowCheckedModeBanner: false,
