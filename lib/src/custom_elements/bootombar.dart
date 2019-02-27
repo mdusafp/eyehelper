ba9b25db13989977ad683ea3a7fe7d71de67bfc9
Anton <kondratyuk.anton.ig@gmail.com>
2019-02-27 09:43:24 +0300

    fix bottom bar

--- lib/src/custom_elements/bootombar.dart
+++ lib/src/custom_elements/bootombar.dart
@@ -8,12 +8,54 @@ class BottomWavy extends StatelessWidget {
       ClipShadowPath(
         clipper: WaveClipper(),
         shadow: Shadow(blurRadius: 10, color: Colors.grey),
-        child: SizedBox(
-          height: 120.0,
-          child: Container(
-              decoration: BoxDecoration(color: Colors.white), child: null),
-        ),
+        child: Container(
+            color: Colors.white,
+            height: 100.0,
+            child: Padding(
+              padding: EdgeInsets.only(left: 30.0, right: 30.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  InkWell(
+                    onTap: () {},
+                    child: Icon(Icons.notifications),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Icon(Icons.notifications),
+                  )
+                ],
+              ),
+            )),
       ),
+      Positioned(
+        bottom: 0.0,
+        child: Container(
+            width: MediaQuery
+                .of(context)
+                .size
+                .width,
+            child: Padding(
+                padding: EdgeInsets.only(bottom: 30.0),
+                child: Material(
+                  type: MaterialType.transparency,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Container(
+                        height: 60.0,
+                        width: 60.0,
+                        child: FittedBox(
+                          child: FloatingActionButton(
+                            heroTag: 'mainBtn',
+                            onPressed: () {},
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ))),
+      )
     ]);
   }
 }
