219ca9a841e4f77336ffbd54dd48567ca35d1bea
Vadim Malyutin <mdusafp@gmail.com>
2020-05-07 07:31:28 +0300

    finish ios firebase config, android still issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+import 'package:flutter/foundation.dart';
 import 'package:eyehelper/src/app.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
@@ -7,7 +7,7 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 void main() {
   ErrorWidget.builder = (FlutterErrorDetails details) => Container();
 
-  Crashlytics.instance.enableInDevMode = true;
+  Crashlytics.instance.enableInDevMode = !kReleaseMode;
 
   // Pass all uncaught errors from the framework to Crashlytics.
   FlutterError.onError = Crashlytics.instance.recordFlutterError;
